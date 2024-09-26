222706ad220d25a32a9b6970b6f47736554cc521
Mabsky <grimdango@gmail.com>
2024-09-26 14:43:53 +1000

    updating backend

--- src/Register.js
+++ src/Register.js
@@ -7,16 +7,33 @@ import { useNavigate } from 'react-router-dom'
     const [password, setPassword0] = useState('')
     const [emailError, setEmailError] = useState('')
     const [passwordError, setPasswordError] = useState('')
-
-
 const navigate = useNavigate()
-
 const onButtonClick = () => {}*/
 
-export default function Register() { return (
+export default function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] =useState("");
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+        let result = await fetch(
+            'http://localhost:3001/register' {
+                method: "post",
+                body: JSON.stringify({name, email}),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+        result = await result.json();
+        console.warn(result);
+        if (result) {
+            alert("Data saved successfully");
+            setEmail("");
+            setName("");
+        }
+    }
 
+    return (
             <div>Register</div>
-    
 )
 
 }
--- src/server.js
+++ src/server.js
@@ -1,16 +1,68 @@
-const express = require ('express');
+//Express setup
+const express = require('express')
 const app = express();
-
+//const port = 5000
 const PORT = process.env.PORT || 3001;
+//const cors = require("cors");
+//app.use(express.json());
+//app.use(cors());
 
+//Localhost Test
+app.get("/", (req, res) => {
+    res.send("App is working");
+})
 
-app.listen(PORT, () => {
-  console.log('Server 3001');
+app.listen(PORT, ()=> {
+    console.log(`App listening at port ${PORT}`)
 });
 
 
-app.get('/', (req, res) => {
-    'Hello from server'
-})
 
+//MongoDB connection setup
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017', {
+    dbname: 'appUsers',
+    useNewURLParser: true,
+    useUnifiedTopology: true, 
 
+}, err => err ? console.log(err) : 
+console.log('Connected to your appUsers database'))
+
+//User Schema
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String, 
+        required: true,
+        unique: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const User = mongoose.model('users', UserSchema);
+User.createIndexes();
+
+app.post ("/register", async (req, res)=> {
+    try {
+        const user = new UserSchema(req.body);
+        let result = await user.save();
+        result = result.toObject();
+        if (result) {
+            delete result.password;
+            res.send(req.body);
+            console.log(result);
+        } else {
+            console.log("User already registered!")
+        }
+    } catch (e) {
+        res.send("Error");
+
+    }
+
+    });
